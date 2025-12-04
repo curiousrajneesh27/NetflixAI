f35c2333796de2aa407a080b5bb98921401b1fb7
rajneesh verma <curiousrajneesh@2027gmail.com>
2025-12-04 13:08:18 +0530

    Security: Move Firebase config to environment variables

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxCFAmVgoyfLgx054q8epCq2hMOq_xxgE",
-  authDomain: "netflixgpt-973f6.firebaseapp.com",
-  projectId: "netflixgpt-973f6",
-  storageBucket: "netflixgpt-973f6.firebasestorage.app",
-  messagingSenderId: "272066354002",
-  appId: "1:272066354002:web:90409bf17cf3245739b88b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
